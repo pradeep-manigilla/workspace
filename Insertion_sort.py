a9cb45f891067b37e4250bcb63a7426ee23d2451
pradeep-manigilla <105184905+pradeep-manigilla@users.noreply.github.com>
2023-05-04 11:58:05 +0530

    Create Insertion_sort.py

---
+++ Insertion_sort.py
@@ -0,0 +1,11 @@
+def sort(a):
+  print ('Initializing Soting Process')
+ def main():
+  a=[]
+  n=int(input('Number of Integers in the array'))
+  for i in range(n):
+    j=int(input())
+    a.append(j)
+  sort(a)
+ if __name__ == '__main__':
+  main()
