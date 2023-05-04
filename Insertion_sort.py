3f2c9346cbfc22054f1cb02e736f11503896028e
pradeep-manigilla <105184905+pradeep-manigilla@users.noreply.github.com>
2023-05-04 08:11:51 +0000

    Insertion_Sort_Algorithm

--- Insertion_sort.py
+++ Insertion_sort.py
@@ -1,11 +1,22 @@
 def sort(a):
   print ('Initializing Soting Process')
- def main():
+  for i in range(1,len(a)):
+     for j in range(i-1, -1, -1):
+        if (j == 0 and a[j] > a[i]) or (a[j-1] <= a[i] and a[j] > a[i]):
+          t = a[i]
+          for k in range(i-1, j-1, -1):
+            a[k + 1] = a[k]
+          a[j] = t
+          break
+  print ('Sorted Array: ', a)
+
+def main():
   a=[]
   n=int(input('Number of Integers in the array'))
   for i in range(n):
-    j=int(input())
-    a.append(j)
+      j=int(input())
+      a.append(j)
   sort(a)
- if __name__ == '__main__':
+
+if __name__ == '__main__':
   main()
